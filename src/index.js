9c36fefd489236777d2c49db395ff80507d641ea
Akshaya Reddy <akshaya.reddy@ibm.com>
2022-04-09 15:58:34 +0530

    filters done

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Main from "./Main";
-import Active from "./Active";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
